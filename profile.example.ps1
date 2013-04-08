d4ed43a77a7e647f26991f82bfc4a9187a84ae94
Christian Ruttorf <chris@oafx.de>
2013-04-08 15:21:28 +0200

    Added Prompt Binding

--- profile.example.ps1
+++ profile.example.ps1
@@ -10,12 +10,43 @@ Import-Module .\posh-git
 
 # Set up a simple prompt, adding the git prompt parts inside git repos
 function prompt {
+
+    #Added prompt binding - Inspired by http://einsteintech.wordpress.com/2009/01/19/a-powershell-prompt-with-a-purpose/
+    #See URL for usage and description.
+
+    [CmdletBinding(DefaultParameterSetName='Prompt')]
+    param ( 
+        
+        [Parameter(ParameterSetName='Minimize')]
+        [Switch] $Minimize,
+        
+        [Parameter(ParameterSetName='Hide')]
+        [Switch] $Hide,
+        
+        [Parameter(ParameterSetName='Show')]
+        [Switch] $Show
+        
+    )
+
     $realLASTEXITCODE = $LASTEXITCODE
 
     # Reset color, which can be messed up by Enable-GitColors
     $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
-    Write-Host($pwd.ProviderPath) -nonewline
+    #Write-Host($pwd.ProviderPath) -nonewline
+
+    switch ( $PSCmdlet.ParameterSetName ) {
+        Minimize { $Global:PromptVisibility = 'Minimized' }
+        Hide     { $Global:PromptVisibility = 'Hidden' }
+        Show     { $Global:PromptVisibility = 'Default' }
+        Prompt   {
+            switch ( $PromptVisibility ) {
+                Minimized { Write-Host("PS $(Split-Path $PWD -Leaf)") -nonewline }
+                Hidden    { Write-Host("PS") -nonewline }
+                Default   { Write-Host("$PWD") -nonewline }
+            }
+        }
+    }
 
     Write-VcsStatus
 
